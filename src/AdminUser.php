d61d5f297e579a233d564445360befbaac5a33f3
Aaron Kaczmarek <aaronmichaelmusic@gmail.com>
2017-09-21 14:14:11 -0400

    refactor admin user setup

--- src/AdminUser.php
+++ src/AdminUser.php
@@ -4,13 +4,53 @@ namespace TheLHC\CustomAuth;
 
 trait AdminUser
 {
+
+    /**
+     * The currently authenticated admin user.
+     *
+     * @var \Illuminate\Contracts\Auth\Authenticatable
+     */
+    protected $adminUser;
+
+    /**
+     * Log the given admin user ID into the application without sessions or cookies.
+     *
+     * @param  mixed  $id
+     * @return bool
+     */
+    public function adminOnceUsingId($id)
+    {
+        if (! is_null($user = $this->provider->retrieveById($id))) {
+            $this->setAdminUser($user);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Set the current user.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return void
+     */
+    public function setAdminUser(UserContract $user)
+    {
+        $this->adminUser = $user;
+    }
+
+    /**
+     * Get the currently authenticated admin user.
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
     public function adminUser()
     {
-        if (! session()->has('adminid')) return null;
+        if (! is_null($this->adminUser)) {
+            return $this->adminUser;
+        }
 
-        $model = $this->provider->getModel();
-        $class = '\\'.ltrim($model, '\\');
-
-        return (new $class)->find(session('adminid'));
+        return;
     }
 }
